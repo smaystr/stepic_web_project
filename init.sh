ecb74a3d6d36d2c895846146854c9984ca7fad40
smaystr <smaystr@gmail.com>
2017-03-07 20:55:06 +0200

    Add: some to gunicorn & nginx

--- init.sh
+++ init.sh
@@ -6,3 +6,7 @@ sudo -s rm /etc/nginx/sites-enabled/default
 
 # restart nginx
 sudo -s /etc/init.d/nginx restart
+sudo -s /etc/init.d/gunicorn restart
+
+# creare symbolic links to gunicorn configs
+sudo -s ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
