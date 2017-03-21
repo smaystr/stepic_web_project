c07a6fdcc291c19488b309df4314876b2f8ffa0a
smaystr <smaystr@gmail.com>
2017-03-21 19:01:34 +0200

    add new conf

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -3,36 +3,72 @@ from django.http import HttpResponse
 
 
 def index(request, *args, **kwargs):
-    return HttpResponse('index - OK')
+    return HttpResponse(
+        content='OK',
+        content_type='html/text',
+        status=200,
+    )
 
 
 def user_login(request, *args, **kwargs):
-    return HttpResponse('user_login - OK')
+    return HttpResponse(
+        content='OK',
+        content_type='html/text',
+        status=200,
+    )
 
 
 def user_logout(request, *args, **kwargs):
-    return HttpResponse('user_logout - OK')
+    return HttpResponse(
+        content='OK',
+        content_type='html/text',
+        status=200,
+    )
 
 
 def user_signup(request, *args, **kwargs):
-    return HttpResponse('user_signup - OK')
+    return HttpResponse(
+        content='OK',
+        content_type='html/text',
+        status=200,
+    )
 
 
 def question(request, *args, **kwargs):
-    return HttpResponse('question - OK')
+    return HttpResponse(
+        content='OK',
+        content_type='html/text',
+        status=200,
+    )
 
 
 def ask(request, *args, **kwargs):
-    return HttpResponse('ask - OK')
+    return HttpResponse(
+        content='OK',
+        content_type='html/text',
+        status=200,
+    )
 
 
 def answer(request, *args, **kwargs):
-    return HttpResponse('answer - OK')
+    return HttpResponse(
+        content='OK',
+        content_type='html/text',
+        status=200,
+    )
 
 
 def popular(request, *args, **kwargs):
-    return HttpResponse('popular - OK')
+    return HttpResponse(
+        content='OK',
+        content_type='html/text',
+        status=200,
+    )
 
 
 def test(request, *args, **kwargs):
-    return HttpResponse('OK')
+    return HttpResponse(
+        content='OK',
+        content_type='html/text',
+        status=200,
+    )
