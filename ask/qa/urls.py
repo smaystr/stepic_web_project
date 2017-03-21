cd09654b35b1a800a19cc4049cd6b01613ccf8c7
smaystr <smaystr@gmail.com>
2017-03-21 13:46:11 +0200

    add new conf

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -17,12 +17,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/.*$', views.user_login, name='login'),
-    url(r'^logout/.*$', views.user_logout, name='logout'),
-    url(r'^signup/.*$', views.user_signup, name='signup'),
-    url(r'^question/(?P<question_id>[0-9]+)/$', views.question, name='question'),
-    url(r'^ask/.*$', views.ask, name='ask'),
-    url(r'^answer/.*$', views.answer, name='answer'),
-    url(r'^popular/.*$', views.popular, name='popular'),
-    url(r'^new/.*$', views.test, name='new'),
+    url(r'^login(/)?.*$', views.user_login, name='login'),
+    url(r'^logout(/)?.*$', views.user_logout, name='logout'),
+    url(r'^signup(/)?.*$', views.user_signup, name='signup'),
+    url(r'^question(/)?(?P<question_id>[0-9]+)/$', views.question, name='question'),
+    url(r'^ask(/)?.*$', views.ask, name='ask'),
+    url(r'^answer(/)?.*$', views.answer, name='answer'),
+    url(r'^popular(/)?.*$', views.popular, name='popular'),
+    url(r'^new(/)?.*$', views.test, name='new'),
 ]
--- init.sh
+++ init.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
 # check & create symbolic link to a new nginx config
+touch /home/box/web/nginx.log
 if [ -f /etc/nginx/sites-enabled/default ]; then
     sudo unlink /etc/nginx/sites-enabled/default
     sudo -s rm /etc/nginx/sites-enabled/default
 fi
-sudo -s ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/django.conf
+sudo -s ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/ask.conf
+
 
 # creare symbolic links to gunicorn configs
-sudo -s ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
-sudo -s ln -sf /home/box/web/etc/gunicorn_ask.conf  /etc/gunicorn.d/gunicorn_ask.conf
+touch /home/box/web/gunicorn.log
+# sudo -s ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
+sudo -s ln -sf /home/box/web/etc/gunicorn_ask.conf  /etc/gunicorn.d/ask
 
-# restart nginx, gunicorn
-sudo -s /etc/init.d/nginx restart
+
+# restart gunicorn, nginx
 sudo -s /etc/init.d/gunicorn restart
+sudo -s /etc/init.d/nginx restart
